ae06b670e02d5cb8a80da0eb8b62d745a34d94ef
Vlad Groza <vlad.andrei.groza@stud.ubbcluj.ro>
2024-03-19 15:17:05 +0200

    Update commit

--- src/test/java/ssvv/example/AppTest.java
+++ src/test/java/ssvv/example/AppTest.java
@@ -1,8 +1,19 @@
 package ssvv.example;
 
+import domain.Nota;
+import domain.Student;
+import domain.Tema;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import repository.NotaXMLRepository;
+import repository.StudentXMLRepository;
+import repository.TemaXMLRepository;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.Validator;
 
 /**
  * Unit test for simple App.
@@ -10,6 +21,16 @@ import junit.framework.TestSuite;
 public class AppTest 
     extends TestCase
 {
+    Validator<Student> studentValidator = new StudentValidator();
+    Validator<Tema> temaValidator = new TemaValidator();
+    Validator<Nota> notaValidator = new NotaValidator();
+
+    StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti_test.xml");
+    TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+    NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+    Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+
     /**
      * Create the test case
      *
@@ -37,10 +58,20 @@ public class AppTest
     }
 
     public void test1() {
+        Iterable<Student> students = service.findAllStudents();
+        int size = 0;
+        for (Student student : students) {
+            size++;
+        }
 
+        int result = service.saveStudent("3", "Andrei", 935);
+
+        assertEquals(0, result);
+        // assertEquals(size + 1, service.findAllStudents().spliterator().getExactSizeIfKnown());
     }
 
     public void test2() {
-
+        int result = service.saveStudent("4", "Dan", 935);
+        assertEquals(1, result);
     }
 }
