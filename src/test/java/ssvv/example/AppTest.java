ecbcd23506c1aa9b7d6dafd22c9568a5f27668ef
Vlad Groza <vlad.andrei.groza@stud.ubbcluj.ro>
2024-03-28 18:00:05 +0200

    Add tests before correct code

--- src/test/java/ssvv/example/AppTest.java
+++ src/test/java/ssvv/example/AppTest.java
@@ -10,10 +10,10 @@ import repository.NotaXMLRepository;
 import repository.StudentXMLRepository;
 import repository.TemaXMLRepository;
 import service.Service;
-import validation.NotaValidator;
-import validation.StudentValidator;
-import validation.TemaValidator;
-import validation.Validator;
+import validation.*;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 /**
  * Unit test for simple App.
@@ -52,26 +52,84 @@ public class AppTest
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 
     public void test1() {
-        Iterable<Student> students = service.findAllStudents();
-        int size = 0;
-        for (Student student : students) {
-            size++;
-        }
-
-        int result = service.saveStudent("3", "Andrei", 935);
-
-        assertEquals(0, result);
-        // assertEquals(size + 1, service.findAllStudents().spliterator().getExactSizeIfKnown());
+        int result = service.saveStudent("13", "NicVlad", 933);
+        assertEquals(1, result);
+        service.deleteStudent("13");
     }
 
     public void test2() {
-        int result = service.saveStudent("4", "Dan", 935);
+        assertThrows(ValidationException.class, () -> this.service.saveStudent(null, "Vlad", 933));
+    }
+
+    public void test3() {
+        assertThrows(ValidationException.class, () -> this.service.saveStudent("", "Vlad", 933));
+    }
+
+    public void test4() {
+        int result = service.saveStudent("2", "NicVlad", 935);
         assertEquals(1, result);
+        service.deleteStudent("2");
+    }
+
+    public void test5() {
+        assertThrows(ValidationException.class, () -> this.service.saveStudent("16", null, 933));
+    }
+
+    public void test6() {
+        assertThrows(ValidationException.class, () -> this.service.saveStudent("15", "", 933));
+    }
+
+    public void test7() {
+        int result = service.saveStudent("3", "NicVlad", 933);
+        assertEquals(1, result);
+        service.deleteStudent("3");
+    }
+
+    public void test8() {
+        int result = this.service.saveStudent("14", "NicVlad", Integer.MAX_VALUE);
+        assertEquals(1, result);
+        this.service.deleteStudent("14");
+    }
+
+
+    public void test9() {
+        assertThrows(ValidationException.class, () -> this.service.saveStudent("117", "NicVlad", -933));
+    }
+
+    public void test10() {
+        assertThrows(ValidationException.class, () -> this.service.saveStudent("13", "NicVlad", Integer.MAX_VALUE + 1));
+    }
+
+    public void test11() {
+        int result =  this.service.saveStudent("16", "Vlad", 0);
+        assertEquals(1, result);
+        this.service.deleteStudent("16");
+    }
+
+
+    public void test12() {
+        int result = this.service.saveStudent("15", "NicVlad", Integer.MAX_VALUE - 1);
+        assertEquals(1, result);
+        this.service.deleteStudent("15");
+    }
+
+    public void test13() {
+        int result = this.service.saveStudent("13", "NicVlad", 935);
+        assertEquals(1, result);
+        this.service.deleteStudent("13");
+    }
+
+    public void test14() {
+        assertThrows(ValidationException.class, () -> this.service.saveStudent("", "", -933));
+    }
+
+    public void test15() {
+        int result = service.saveStudent("13", "NicVlad", 935);
+        assertEquals(1, result);
+        result = service.saveStudent("13", "NicVlad", 935);
+        assertEquals(0, result);
+        service.deleteStudent("13");
     }
 }
